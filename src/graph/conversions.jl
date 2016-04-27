812bfbda432138d2bc21c0ffa36e52537366730f
Mike J Innes <mike.j.innes@gmail.com>
2016-04-27 18:33:36 +0100

    may as well make this well typed

--- src/graph/conversions.jl
+++ src/graph/conversions.jl
@@ -4,7 +4,7 @@ convert{T}(::Type{Needle{T}}, n::Needle) =
   Needle{T}(convert(T, n.vertex), n.output)
 
 for (V, W) in [(DLVertex, ILVertex), (ILVertex, DLVertex)]
-  @eval function convert{T}(::Type{$W{T}}, v::$V, cache = d())
+  @eval function convert{T}(::Type{$W{T}}, v::$V, cache = Dict{typeof(v),$W{T}}())
     haskey(cache, v) && return cache[v]
     w = cache[v] = $W{T}(value(v))
     thread!(w, [Needle{$W{T}}(convert($W{T}, n.vertex, cache), n.output) for n in inputs(v)]...)
