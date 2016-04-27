72031fc31a742aba963f33a377796a9b2ba1afbf
Mike J Innes <mike.j.innes@gmail.com>
2016-04-27 18:28:22 +0100

    better default types

--- src/graph/conversions.jl
+++ src/graph/conversions.jl
@@ -1,10 +1,13 @@
-Base.convert{T}(::Type{Needle{T}}, n::Needle) =
+import Base: convert
+
+convert{T}(::Type{Needle{T}}, n::Needle) =
   Needle{T}(convert(T, n.vertex), n.output)
 
 for (V, W) in [(DLVertex, ILVertex), (ILVertex, DLVertex)]
-  @eval function Base.convert{W<:$W}(::Type{W}, v::$V, cache = d())
+  @eval function convert{T}(::Type{$W{T}}, v::$V, cache = d())
     haskey(cache, v) && return cache[v]
-    w = cache[v] = W(value(v))
-    thread!(w, [Needle{W}(convert(W, n.vertex, cache), n.output) for n in inputs(v)]...)
+    w = cache[v] = $W{T}(value(v))
+    thread!(w, [Needle{$W{T}}(convert($W{T}, n.vertex, cache), n.output) for n in inputs(v)]...)
   end
+  @eval convert(::Type{$W}, v::AVertex) = convert($W{eltype(v)}, v)
 end
