1d60978466f040d839ffd3ad4a9da78ce00434aa
Mike J Innes <mike.j.innes@gmail.com>
2016-04-28 01:39:11 +0100

    avoid doing CSE when converting

--- src/graph/conversions.jl
+++ src/graph/conversions.jl
@@ -4,7 +4,7 @@ convert{T}(::Type{Needle{T}}, n::Needle) =
   Needle{T}(convert(T, n.vertex), n.output)
 
 for (V, W) in [(DLVertex, ILVertex), (ILVertex, DLVertex)]
-  @eval function convert{T}(::Type{$W{T}}, v::$V, cache = Dict{typeof(v),$W{T}}())
+  @eval function convert{T}(::Type{$W{T}}, v::$V, cache = ODict())
     haskey(cache, v) && return cache[v]
     w = cache[v] = $W{T}(value(v))
     thread!(w, [Needle{$W{T}}(convert($W{T}, n.vertex, cache), n.output) for n in inputs(v)]...)
