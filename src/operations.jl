8a07293714ef6a2c9a4c26c5df72a54dc9e539fc
Mike J Innes <mike.j.innes@gmail.com>
2016-04-28 16:05:43 +0100

    tweak

--- src/operations.jl
+++ src/operations.jl
@@ -1,6 +1,4 @@
-function cse(v::IVertex)
-  cache = Dict{eltype(v),eltype(v)}()
+cse(v::IVertex, cache = Dict{eltype(v),eltype(v)}()) =
   postwalk(x -> get!(cache, x, x), v)
-end
 
 cse(v::AVertex) = cse(il(v))
