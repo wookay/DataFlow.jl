86b57c2c70d2b363a9157d9b6b25b8aea87050ea
Mike J Innes <mike.j.innes@gmail.com>
2016-05-03 17:52:09 +0100

    dvertex equality

--- src/graph/dlgraph.jl
+++ src/graph/dlgraph.jl
@@ -23,3 +23,22 @@ end
 vertex(a...) = DVertex{Any}(a...)
 
 dl(v::Vertex) = convert(DVertex, v)
+
+function equal(a::Vertex, b::Vertex, seen = OSet())
+  (a, b) ∈ seen && return true
+  (value(a) == value(b) &&
+    length(inputs(a)) == length(inputs(b)) &&
+    length(outputs(a)) == length(outputs(b))) || return false
+  push!(seen, (a, b))
+  for (i, j) in zip(inputs(a), inputs(b))
+    equal(i, j, seen) || return false
+  end
+  @assert @>> a outputs map(value) allunique
+  @assert @>> b outputs map(value) allunique
+  for o in outputs(a)
+    p = filter(p -> value(p) == value(o), outputs(b))
+    isempty(p) && return false
+    equal(o, first(p), seen) || return false
+  end
+  return true
+end
