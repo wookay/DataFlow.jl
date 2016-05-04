48a77c649f86979a6c5dd8b8afeb1558cfa46b44
Mike J Innes <mike.j.innes@gmail.com>
2016-05-04 20:54:10 +0100

    initial test

--- src/Flow.jl
+++ src/Flow.jl
@@ -5,5 +5,6 @@ using Lazy, MacroTools
 include("graph/graph.jl")
 include("syntax.jl")
 include("operations.jl")
+include("fuzz.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,12 @@
-using Flow
-using Base.Test
+using Flow, Flow.Fuzz
+using Lazy, Base.Test
+
+import Flow: equal, graphm, syntax
+
+for nodes = 1:10, tries = 1:1_000
+
+dl = grow(DVertex, nodes)
+
+@test @> dl syntax(flatconst = false) graphm equal(dl)
+
+end
