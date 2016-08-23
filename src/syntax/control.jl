399f4fd2e1afdf2bb992689389b9acfcf8ebcc39
Mike J Innes <mike.j.innes@gmail.com>
2016-08-23 12:52:18 +0100

    equality for Constants

--- src/syntax/control.jl
+++ src/syntax/control.jl
@@ -1,3 +1,5 @@
+import Base: ==
+
 export Constant, constant, dconstant, isconstant
 
 immutable Constant{T}
@@ -9,6 +11,10 @@ tocall(c::Constant) = c.value
 isconstant(v::Vertex) = isa(value(v), Constant)
 
 mapconst(f, g) = map(x -> isa(x, Constant) ? Constant(f(x.value)) : f(x), g)
+
+a::Constant == b::Constant = a.value == b.value
+
+Base.hash(c::Constant, h::UInt = UInt(0)) = hash((Constant, c.value), h)
 
 for (c, v) in [(:constant, :vertex), (:dconstant, dvertex)]
   @eval $c(x) = $v(Constant(x))
