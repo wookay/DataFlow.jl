899491ae0fd6ed12f0f3e12210ea8ac673c36e02
Mike J Innes <mike.j.innes@gmail.com>
2016-11-13 20:26:28 +0000

    abstract interpreter pieces

--- src/DataFlow.jl
+++ src/DataFlow.jl
@@ -5,6 +5,7 @@ using Lazy, MacroTools, Juno
 include("graph/graph.jl")
 include("syntax/syntax.jl")
 include("operations.jl")
+include("interpreter.jl")
 include("fuzz.jl")
 
 end # module
---
+++ src/interpreter.jl
@@ -0,0 +1,41 @@
+# TODO: stack and error messages
+
+type Context{T}
+  interp::T
+  cache::ObjectIdDict
+  data::Dict{Symbol,Any}
+end
+
+Context(interp; kws...) = Context(interp, ObjectIdDict(), Dict{Symbol,Any}(kws))
+
+Base.getindex(ctx::Context, k::Symbol) = ctx.data[k]
+Base.setindex!(ctx::Context, v, k::Symbol) = ctx.data[k] = v
+
+function interpret(ctx::Context, graph::IVertex, inputs...)
+  graph = spliceinputs(graph, map(constant, inputs)...)
+  interpret(ctx, graph)
+end
+
+function interpret(ctx::Context, graph::IVertex)
+  haskey(ctx.cache, graph) && return ctx.cache[graph]
+  ctx.cache[graph] = ctx.interp(ctx, value(graph), inputs(graph)...)
+end
+
+interpret(ctx::Context, xs::Tuple) = map(x -> interpret(ctx, x), xs)
+
+# Composable interpreter pieces
+
+function interpconst(f)
+  interp(ctx::Context, x::Constant) = x.value
+  interp(ctx::Context, xs...) = f(ctx, xs...)
+end
+
+function interptuple(f)
+  interp(ctx::Context, ::Group, xs...) = tuple(interpret(ctx, xs)...)
+  interp(ctx::Context, s::Split, xs) = interpret(ctx, xs)[s.n]
+  interp(ctx::Context, xs...) = f(ctx, xs...)
+end
+
+interpnull(ctx, f, xs...) = vertex(f, interpret(ctx, xs)...)
+
+const interpeval = interpconst(interptuple((ctx, f, xs...) -> f(interpret(ctx, xs)...)))
